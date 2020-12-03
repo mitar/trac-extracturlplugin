a8b85728c9f14064802f64799512101f59106061
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2020-12-03 21:44:39 +0000

    TracExtractUrl 0.5: Remove `www` prefix from URL
    git-svn-id: https://trac-hacks.org/svn/extracturlplugin@17892 7322e99d-02ea-0310-aa39-e9a107903beb

--- 0.11/setup.py
+++ 0.11/setup.py
@@ -5,12 +5,12 @@ from setuptools import setup
 
 setup(
     name         = 'TracExtractUrl',
-    version      = '0.4',
+    version      = '0.5',
     packages     = ['tracextracturl'],
     author       = 'Martin Scharrer',
     author_email = 'martin@scharrer-online.de',
     description  = 'Provides `extract_url` method to extract the URL from TracWiki links.',
-    url          = 'https://www.trac-hacks.org/wiki/ExtractUrlPlugin',
+    url          = 'https://trac-hacks.org/wiki/ExtractUrlPlugin',
     download_url = 'https://pypi.python.org/pypi/TracExtractUrl',
     license      = 'GPLv3',
     keywords     = 'trac plugin extract url',
